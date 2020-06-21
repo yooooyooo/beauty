5d0ce8b63366748c97ca7e80eae381b114dcf034
游佳弘 <youjiahong@MacBook-Pro.local>
2020-06-21 21:54:47 +0800

    create superuser, novel model.

--- boy/novels/admin.py
+++ boy/novels/admin.py
@@ -1,3 +1,12 @@
+"""admin configuration of novels."""
 from django.contrib import admin
 
-# Register your models here.
+from boy.novels.models import Novel
+
+
+@admin.register(Novel)
+class NovelAdmin(admin.ModelAdmin):
+    """admin config of Novel model."""
+    list_display = ('id', 'name', 'author', 'rank', 'comment', 'updated')
+    search_fields = ('name', 'author')
+    list_filter = ('rank', 'updated')
--- boy/novels/models.py
+++ boy/novels/models.py
@@ -1,3 +1,13 @@
+"""novels models."""
 from django.db import models
 
-# Create your models here.
+
+class Novel(models.Model):
+    """novel model."""
+    name = models.CharField(max_length=16, unique=True)
+    author = models.CharField(max_length=8)
+    rank = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    comment = models.IntegerField(default=0, help_text='number of comments.')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
